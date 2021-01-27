d5ef085451de305cbc8b6e2bbe45866d9b63aba8
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-01-27 14:27:54 -0700

    Create test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -2,19 +2,15 @@
 
 mkdir -p srv/mediawiki
 
-#ln -sf /srv /home/runner/work/mediawiki/mediawiki/srv
-
 cd srv/mediawiki
+
+composer self-update --1
 
 /usr/bin/git clone https://github.com/miraheze/mw-config.git config --depth=1
 
-/usr/bin/git clone https://github.com/miraheze/mediawiki.git w --recurse-submodules --depth=1
+/usr/bin/git clone https://github.com/miraheze/mediawiki.git w --depth=1
 
 cd w
-
-composer self-update --1
-composer install
-php maintenance/install.php --dbtype sqlite --dbuser root --dbname mw --dbpath $(pwd) --pass AdminPassword WikiName AdminUser
 
 cat <<'EOT' >> composer.local.json
 {
@@ -43,12 +39,17 @@ cat <<'EOT' >> composer.local.json
 }
 EOT
 
+composer install
+php maintenance/install.php --dbtype sqlite --dbuser root --dbname mw --dbpath $(pwd) --pass AdminPassword WikiName AdminUser
+
 rm LocalSettings.php
 cd ..
 mv config/LocalSettings.php w/LocalSettings.php
 cd w
 
-echo -e "<?php set_include_path('/home/runner/work/mediawiki/mediawiki:/home/runner/work/mediawiki/mediawiki/srv/mediawiki/w/vendor/pear/console_getopt:/home/runner/work/mediawiki/mediawiki/srv/mediawiki/w/vendor/pear/mail:/home/runner/work/mediawiki/mediawiki/srv/mediawiki/w/vendor/pear/mail_mime:/home/runner/work/mediawiki/mediawiki/srv/mediawiki/w/vendor/pear/net_smtp:/home/runner/work/mediawiki/mediawiki/srv/mediawiki/w/vendor/pear/net_socket:/home/runner/work/mediawiki/mediawiki/srv/mediawiki/w/vendor/pear/pear-core-minimal/src:/home/runner/work/mediawiki/mediawiki/srv/mediawiki/w/vendor/pear/pear_exception:.:/usr/share/php'); ?>\n$(cat LocalSettings.php)" > LocalSettings.php
+#echo -e "<?php set_include_path( get_include_path() . PATH_SEPARATOR . '/home/runner/work/mediawiki/mediawiki' ); ?>\n$(cat LocalSettings.php)" > LocalSettings.php
+
+sed -i -e 's//srv//home/runner/work/mediawiki/mediawiki//g' LocalSettings.php
 
 echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
 echo 'ini_set("display_errors", 1);' >> LocalSettings.php
