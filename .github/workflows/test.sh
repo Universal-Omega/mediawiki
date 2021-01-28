484bc8e78b85b0b100b2f64754d7e8e19bf5270d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-01-28 15:12:49 -0700

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,20 +1,4 @@
 #! /bin/bash
-readonly EXPECTED_ARGS=3
-readonly E_BADARGS=65
-readonly MYSQL=`which mysql`
-
-readonly Q1="CREATE DATABASE IF NOT EXISTS $1;"
-readonly Q2="GRANT ALL ON *.* TO '$2'@'localhost' IDENTIFIED BY '$3';"
-readonly Q3="FLUSH PRIVILEGES;"
-readonly SQL="${Q1}${Q2}${Q3}"
-
-if [ $# -ne $EXPECTED_ARGS ]
-then
-  echo "Usage: $0 dbname dbuser dbpass"
-  exit $E_BADARGS
-fi
-
-$MYSQL -uroot -p -e "$SQL"
 
 mkdir -p srv/mediawiki
 
